0e845749187847256e61acb433ef7e140b648472
AdrianJB <adrian.jimenez.blanes@gmail.com>
2015-02-25 20:05:07 +0100

    inicio

---
+++ wsgi/openshift/LoLpossessions/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+ 
+class Campeon(models.Model):
+    Nombre = models.CharField(max_length = 30)
+    Titulo = models.CharField(max_length = 50)
+    Icono = models.URLField(max_length = 200)
+    Splashart = models.URLField(max_length = 200)
+    Spotlight = models.URLField(max_length = 200)
+    Habilidades = models.URLField(max_length = 200)
+    Historia = models.URLField(max_length = 200)
+    def __unicode__(self):
+        return self.Nombre
+
+class PosesionCampeon(models.Model):
+    Usuario = models.ForeignKey(User)
+    Campeon = models.ForeignKey(Campeon)
+    Posesion = models.BooleanField(default=False)
+
+admin.site.register(Campeon)
+admin.site.register(PosesionCampeon)
